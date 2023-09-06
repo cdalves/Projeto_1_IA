29feba6ee66c95b4367eb1e4262699d54834d672
cdalves <33464710+cdalves@users.noreply.github.com>
2023-09-05 21:02:47 -0300

    read txt

--- main.cpp
+++ main.cpp
@@ -1,11 +1,45 @@
 #include <iostream>
-#include <fstream>
 #include <vector>
+#include <string>
+#include <cstdio>
 
 using namespace std;
 
+
+
 int main(){
-    cout<< "ola mundo"; 
+    int m = 0; //número de nós
+    int n = 0; //número de arestas
+    vector<vector<string>> grafo; //recebe os dados do grafo.txt
+    float gs = 0; // salva o peso entre os nós 
+    char o[500];
+    vector <string> a;
+    FILE* arquivo = fopen("grafo.txt","r");
+    
+    if (arquivo)
+    {
+        while (!feof(arquivo))
+        {
+            fgets(o,500 , arquivo);
+            a.push_back(o);            
+        }
+    
+    for(int x= 0; x <= a.size(); x++){
+        cout<<a[x];
+    }
+        
+        
+    }
+    
+        
+    
+    
+    
+    
+  
+
+    
+
 
     return 0;
 }
