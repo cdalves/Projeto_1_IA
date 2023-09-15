a3630ce22620ebc44a98872766737b3bd45e5dc7
cdalves <33464710+cdalves@users.noreply.github.com>
2023-09-14 21:00:26 -0300

    lendo o txt

--- main.cpp
+++ main.cpp
@@ -1,53 +1,54 @@
 #include <iostream>
+#include <fstream>
 #include <vector>
-#include <string>
-#include <cstdio>
-#include <cstdlib>
 
 using namespace std;
 
+int main() {
+    std::ifstream arquivo("grafo.txt"); // Substitua "seuarquivo.txt" pelo nome do seu arquivo
+    int m = 0, n = 0;
 
+    if (!arquivo.is_open()) {
+        std::cerr << "Erro ao abrir o arquivo." << std::endl;
+        return 1;
+    }
 
-int main(){
-    int m = 0; //número de nós
-    int n = 0; //número de arestas
-    vector<vector<string>> grafo; //recebe os dados do grafo.txt
-    float gs = 0; // salva o peso entre os nós 
-    char o[500];
-    vector <string> a;
-    FILE* arquivo = fopen("grafo.txt","r");
-    
-    if (arquivo)
-    {
-        while (!feof(arquivo))
-        {
-            fgets(o,500, arquivo);
-            a.push_back(o);
-           
+    std::vector<std::vector<int>> grafo;
+    int linha = 1;
+    int numero;
+
+    while (arquivo >> numero) {
+        if(m == 0)
+            m = numero;
+            else if(n == 0)
+                 n = numero;
+
+         else if (linha < 2) {
+            // Armazenar os dois primeiros números em uma linha separada
+            grafo.push_back({numero});
+        } else {
+            // Armazenar os números restantes em grafo de três
+            if (grafo.empty() || grafo.back().size() == 3) {
+                grafo.push_back({numero});
+            } else {
+                grafo.back().push_back(numero);
+            }
         }
-        m = stoi(a[0]);
-        n = stoi(a[1]);
-
-
-    
-    for(int x= 0; x <= a.size(); x++){
-        cout<<a[x]<< endl;
+        linha++;
     }
-        cout << m << endl;
-        cout << n << endl;
-
-        
-    }
-    
-        
-    
-    
-    
-    
+    arquivo.close();
   
 
-    
+    cout<< m << endl;
+    cout<< n << endl;
 
+    // Imprimir os grafo de três números restantes
+    for (size_t i = 0; i < grafo.size(); i++) {
+        for (size_t j = 0; j < grafo[i].size(); j++) {
+            cout << " " << grafo[i][j];
+        }
+      cout << std::endl;
+    }
 
     return 0;
 }
--- test.cpp
+++ test.cpp
@@ -1,12 +1,6 @@
-#ifndef LARGURA  
-#define LARGURA
-
-#include <iostream>
-#include <string>
+#include<iostream>
 #include <cstdlib>
-#include <vector>
-
-
+#include<vector>
 using namespace std;
 
 bool testaObjetivo(int atual,int objetivo){
@@ -16,13 +10,38 @@ bool testaObjetivo(int atual,int objetivo){
 		return false;
 }
 
-string buscLargura(int grafo[][100], int inicial, int objetivo) {
+
+int main()
+{
+	int inicial,atual,objetivo,filho;
 	vector<int>listaVisitados, listaVisitar,copia,imprimeCaminho;
-	
-	string result;
-	int filho;
-	int atual = inicial;
 	int mPai[10]={100,100,100,100,100,100,100,100,100,100};
+	
+	cout<< "Digite o valor do estado inicial\n";
+	cin >> inicial;
+	system("cls");
+	cout<< "Digite o valor do estado objetivo\n";
+	cin >> objetivo;
+	system("cls");
+	
+
+	int grafo[10][10];
+	//Matriz
+	{
+		grafo[0][0]=0;grafo[0][1]=1;grafo[0][2]=0;grafo[0][3]=1;grafo[0][4]=0;grafo[0][5]=0;grafo[0][6]=0;grafo[0][7]=0;grafo[0][8]=0;grafo[0][9]=0;
+		grafo[1][0]=1;grafo[1][1]=0;grafo[1][2]=1;grafo[1][3]=0;grafo[1][4]=0;grafo[1][5]=0;grafo[1][6]=0;grafo[1][7]=0;grafo[1][8]=0;grafo[1][9]=0;
+		grafo[2][0]=0;grafo[2][1]=1;grafo[2][2]=0;grafo[2][3]=1;grafo[2][4]=0;grafo[2][5]=0;grafo[2][6]=0;grafo[2][7]=0;grafo[2][8]=0;grafo[2][9]=0;
+		grafo[3][0]=1;grafo[3][1]=0;grafo[3][2]=1;grafo[3][3]=0;grafo[3][4]=1;grafo[3][5]=0;grafo[3][6]=0;grafo[3][7]=1;grafo[3][8]=0;grafo[3][9]=0;
+		grafo[4][0]=0;grafo[4][1]=0;grafo[4][2]=0;grafo[4][3]=1;grafo[4][4]=0;grafo[4][5]=1;grafo[4][6]=0;grafo[4][7]=0;grafo[4][8]=0;grafo[4][9]=0;
+		grafo[5][0]=0;grafo[5][1]=0;grafo[5][2]=0;grafo[5][3]=0;grafo[5][4]=1;grafo[5][5]=0;grafo[5][6]=1;grafo[5][7]=0;grafo[5][8]=0;grafo[5][9]=0;
+		grafo[6][0]=0;grafo[6][1]=0;grafo[6][2]=0;grafo[6][3]=0;grafo[6][4]=0;grafo[6][5]=1;grafo[6][6]=0;grafo[6][7]=1;grafo[6][8]=0;grafo[6][9]=0;
+		grafo[7][0]=0;grafo[7][1]=0;grafo[7][2]=0;grafo[7][3]=1;grafo[7][4]=0;grafo[7][5]=0;grafo[7][6]=1;grafo[7][7]=0;grafo[7][8]=0;grafo[7][9]=0;
+		grafo[8][0]=0;grafo[8][1]=0;grafo[8][2]=0;grafo[8][3]=0;grafo[8][4]=0;grafo[8][5]=0;grafo[8][6]=0;grafo[8][7]=0;grafo[8][8]=0;grafo[8][9]=1;
+		grafo[9][0]=0;grafo[9][1]=0;grafo[9][2]=0;grafo[9][3]=0;grafo[9][4]=0;grafo[9][5]=0;grafo[9][6]=0;grafo[9][7]=0;grafo[9][8]=1;grafo[9][9]=0;
+	}
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	atual = inicial;
+	
 	while(true){  //BUSCA EM LARGURA
 		
 		listaVisitados.push_back(atual);
@@ -93,7 +112,7 @@ string buscLargura(int grafo[][100], int inicial, int objetivo) {
 		}
 	  	
 	}
-	return result;
-}
 
-#endif
+	cout << "\n\n";
+	return 0;	
+}
