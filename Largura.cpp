73e88015ec9315616aff19e334b30031e5245a68
cdalves <33464710+cdalves@users.noreply.github.com>
2023-09-20 22:34:43 -0300

    .

--- Largura.cpp
+++ Largura.cpp
@@ -7,16 +7,18 @@ using namespace std;
 
 
 int main() {
-    ifstream arquivo("grafo.txt"); // Substitua "seuarquivo.txt" pelo nome do seu arquivo
+    ifstream arquivo("grafo.txt"); // cria um ponteiro para acessar o arquivo de texto
     int m = 0 , n=0 , objetivo;
     int inicial, numero;
     vector<vector<int>> listadj;  
   
-    if(!arquivo.is_open()) {
+   // indentifica se or arquivo foi aberto corretamente
+    if(!arquivo.is_open()) { // 
         cerr << "Erro ao abrir o arquivo." << endl;
         return 1;
     }
    
+   // salva o arquivo em um vetor no formato desejado
     while (arquivo >> numero) {
         if(m == 0){ // salva o numero de nós
             m = numero;
@@ -24,7 +26,7 @@ int main() {
             else if(n == 0){ // salva o numero de arestas
                  n = numero;
             }
-            else if (listadj.empty() || listadj.back().size() == 3) {
+            else if (listadj.empty() || listadj.back().size() == 3) { 
                     listadj.push_back({numero});
                     
                 }else {
@@ -36,27 +38,29 @@ int main() {
  
     int grafo[m+1][m+1] = {0};
 
+    // tranforma a lista de adjacência em matriz de adjacência
     for(int i = 0; i < listadj.size(); i++) {
             grafo[listadj[i][0]][listadj[i][1]] = listadj[i][2];
     }   
 
-    // Imprimir os grafo de três números restantes
-    cout<< m << endl;
-    cout<< n << endl;
-    for (int i = 1; i < m+1 ; i++) {
-        for (int j = 1; j < m+1; j++) {
-            cout << " " << grafo[i][j]; 
-        }
-      cout << endl;
-    }
+    // // Imprimir a matriz
+    // cout<< m << endl;
+    // cout<< n << endl;
+    // for (int i = 1; i < m+1 ; i++) {
+    //     for (int j = 1; j < m+1; j++) {
+    //         cout << " " << grafo[i][j]; 
+    //     }
+    //   cout << endl;
+    // }
 
+    // solicita ao usuario o no de inicio e de fim
     cout<< "Digite o valor do estado inicial\n";
 	    cin >> inicial;
 	cout<< "Digite o valor do estado objetivo\n";
 	    cin >> objetivo;
     cin.ignore();
     
-////////////////////////////////////////////////////////////////
+/////////////////////  algotitimo de busca em largura  ///////////////////////////////////////////
 
     vector <int> pai(m,-1);
     queue <int> avisitar;
@@ -69,10 +73,10 @@ int main() {
     int atual = avisitar.front();
     avisitar.pop();
 
-    if (atual == objetivo) {
+    if (atual == objetivo) { // verifica se ja chegou ao destino
         vector<int> caminho;
 
-        while (atual != -1) { // A raiz do caminho será -1.
+        while (atual != -1) { // cria a caminho ate chegar ao objetivo
             caminho.push_back(atual);
             atual = pai[atual];
         }
@@ -87,7 +91,7 @@ int main() {
         return 0;
     }
 
-    for (int i = 1; i <= m; i++) {
+    for (int i = 1; i <= m; i++) { // percorre a matriz e enconta os nos filhos
             if (grafo[atual][i] != 0 && pai[i] == -1) {
                 avisitar.push(i);
                 pai[i] = atual;                
